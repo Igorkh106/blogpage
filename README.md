32a5f8e2ddd0f70a0282555d5a4dc92d6154bf6e
i.khrypak <i.khrypak@relevant.sgtware>
2017-01-12 13:34:45 +0200

    Finished comments Added some logic for delete,edit, reply Added font-awesome Little refactor of the code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Frontend-devil
-Devil boilerplate for html+js+css apps. Почитать как это готовить можно [тут](http://habrahabr.ru/post/250569/ "Readme")
+# Test Blog Page
+
+Compiled project is in the /build directory.
+
+Seems like the Stag font is paid and I didn't found it with the task, so replaced with Helvetica.
+
+Also font-awesome is used for the icons.
+
+The main coding part is not ready for now, only a few interface features like show form on reply or edit your comment and delete it. 
+
+Hope this is enough :)
--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,40 @@
 /*
     Custom
  */
-//= partials/helper.js
+//= partials/functions.js
+
+ $(document).ready(function(){
+     "use strict";
+     //show options on hover
+    $(".comment").hover(function(){
+        $(this).find(".comment-options").slideToggle();
+    });
+
+     //delete comment
+     $(".comment-option-delete").click(function(){
+       var parentComment = $(this).closest(".comment");
+         parentComment.hide();
+
+     });
+
+     //open edit form for a comment
+     $(".comment-option-edit").click(function(){
+         var parentComment = $(this).closest(".comment");
+         var commentText = parentComment.find("p").text();
+         var editForm = addEditForm(commentText);
+
+         !parentComment.find("form").length ? parentComment.append(editForm) : false;
+
+     });
+
+     //open reply form for a comment
+     $(".comment-option-reply").click(function(){
+         var parentComment = $(this).closest(".comment");
+         var replyTo = parentComment.find(".comment-author-name").text();
+         var replyForm = addReplyForm(replyTo);
+
+         !parentComment.find("form").length ? parentComment.append(replyForm) : false;
+
+     });
+
+ });
